eabe81b60e88288448ae135369bc7023cde6ff3f
waspyru <waspyru@gmail.com>
2021-12-05 19:01:29 +0300

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+import Web from 'steamatic'
+import minitmist from 'minimist'
+
+const args = minitmist(process.argv.splice(2))
+args.L = 'mamozor'
+const web = await Web.Basic(args, console.log)
+web.requestHooks.beforeRequest.push((opts, next) => {
+  console.log('>', opts.request.url.toString())
+  next()
+})
+
+while(true) {
+  await web.trade.poll()
+  const donation = Array.from(web.trade.activeOffers.values()).find(offer => offer.items.our.length === 0)
+  if(!donation) break
+  else await donation.accept()
+}
+
+
+
+
+
