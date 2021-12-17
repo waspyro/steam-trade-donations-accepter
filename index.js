2fa9022224d05e5294e4dd4d9e379f4d7e1bc746
waspyru <waspyru@gmail.com>
2021-12-18 01:10:04 +0300

    some fixes and new --loop option for infinite polling

--- index.js
+++ index.js
@@ -9,10 +9,24 @@ web.requestHooks.beforeRequest.push((opts, next) => {
 })
 
 while(true) {
-  await web.trade.poll()
-  const donation = Array.from(web.trade.activeOffers.values()).find(offer => offer.items.our.length === 0)
-  if(!donation) break
-  else await donation.accept()
+  const {active} = await web.trade.poll()
+  const donations = active.donations
+  console.log('donations left', donations.length)
+  const donation = donations[0]
+  if(!donation) { //make infinite poll loop if user wants to accept all incoming offers
+    let time = args.l || args.loop
+    if(!time) break
+    if(time === true) time = 30000
+    await new Promise(resolve => setTimeout(resolve, Number(time)))
+    continue
+  }
+  const {completed, confirmationRequired} = await donation.accept()
+  if(completed) continue
+  if(confirmationRequired)
+    throw new Error('donation offer should not require confirmation, something gone wrong')
+  //this probably some large offer, should wait some time till it's completed
+  console.log('accepting this offer may require some time')
+  await new Promise(resolve => donation.setSyncInterval(10000).once('completed', resolve))
 }
 
 
